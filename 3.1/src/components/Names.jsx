e24748a1613620c0adb6a3b1f1e2a7c3b5704359
Daniel Kamenetsky <20dk19@queensu.ca>
2024-10-16 15:42:34 -0400

    Remove deleted files from tracking

--- 3.1/src/components/Names.jsx
+++
@@ -1,8 +0,0 @@
-export const Names = ({ person, deleteName }) => {
-  return (
-    <div>
-      {person.name} {person.number}{" "}
-      <button onClick={() => deleteName(person.id, person.name)}>delete</button>
-    </div>
-  );
-};
