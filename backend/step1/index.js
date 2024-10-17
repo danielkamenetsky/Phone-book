309a9da0bb3dbf25d29c1ccbfc7574ad5a82854c
Daniel Kamenetsky <20dk19@queensu.ca>
2024-10-16 23:01:36 -0400

    feat: Implement basic phonebook API with GET, DELETE endpoints
    - Added /api/persons to display all persons
    - Added /api/persons/:id to display a single person by id
    - Added DELETE /api/persons/:id to remove a person by id
    - Created /info endpoint to show total persons and timestamp
    - Ensured correct deletion logic by fixing id comparison and updating persons array

--- backend/step1/index.js
+++ backend/step1/index.js
@@ -8,22 +8,22 @@ const app = express()
 let persons = 
 [
   { 
-    "id": "1",
+    "id": 1,
     "name": "Arto Hellas", 
     "number": "040-123456"
   },
   { 
-    "id": "2",
+    "id": 2,
     "name": "Ada Lovelace", 
     "number": "39-44-5323523"
   },
   { 
-    "id": "3",
+    "id": 3,
     "name": "Dan Abramov", 
     "number": "12-43-234345"
   },
   { 
-    "id": "4",
+    "id": 4,
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122"
   }
@@ -34,6 +34,30 @@ app.get("/api/persons", (request, response)=> {
   response.send(persons);
 });
 
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person=>person.id === id)
+  if (!person) {
+    response.status(404).send(`Person with id:${id} is NOT FOUND`)
+  }
+  response.send(person)
+})
+
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  let deletedPerson = persons.filter((person) => person.id !== id)
+  response.send(deletedPerson)
+})
+
 app.listen(3001, ()=> {
   console.log('Server is running!')
+})
+
+app.get("/info", (request, response)=> {
+  response.send(
+    `Phonebook has info for ${persons.length} people. <br> ${Date()}`
+  )
+
 })
